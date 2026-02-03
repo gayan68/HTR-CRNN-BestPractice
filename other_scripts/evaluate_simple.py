13fd4eb73584852bbdc32dbf228ba0fad20f0c3a
gayan68 <gayan68@yahoo.com>
2026-02-03 17:56:14 +0100

    eval

--- other_scripts/evaluate_simple.py
+++ other_scripts/evaluate_simple.py
@@ -159,8 +159,6 @@ def parse_args(file_name):
 
     OmegaConf.set_struct(conf, True)
 
-    #sys.argv = [sys.argv[0]] + sys.argv[2:] # Remove the configuration file name from sys.argv
-
     conf.merge_with_cli()
     return conf
 
@@ -187,79 +185,16 @@ def test_all_models(vs_mdl, dataset_name):
     res_eval = htr_eval.test(0, dataset_name)
     return res_eval
 
-#Read and store model names from the saved direcoty
-#results_cer_df = pd.read_csv("results_old_1/results_cer.csv")
-#results_wer_df = pd.read_csv("results_old_1/results_wer.csv")
-
-
-#saved_directory = config.model_save_dir
-
-#p = saved_directory.strip().split('_')
-#dir = f"{p[-2]}_{p[-1]}"
-
-print (dir)
-
-# saved_models = []
-# for filename in os.listdir(saved_directory):
-#     if os.path.isfile(os.path.join(saved_directory, filename)):
-#         if (filename != f'results_test_{dir}.pkl') and (filename != f'results_val_{dir}.pkl') and (filename != 'logfile.txt'):
-#             saved_models.append(filename)
-# saved_models = sorted(saved_models, key=natural_key)
 
 saved_models = "htrnet_best_cer.pt"
 
 
 
-print(saved_models)
-
-# results_test = test_all_models(saved_models, 'test')
-# #results_val = test_all_models(saved_models, 'val')
-
-# with open(f'{saved_directory}/results_test_{dir}.pkl', 'wb') as file:
-#     pickle.dump(results_test, file)
-# with open(f'{saved_directory}/results_val_{dir}.pkl', 'wb') as file:
-#     pickle.dump(results_val, file)
-
-
-
+"""
+The Input for test_dataset should be one of the following:
 df_splits = ["CLEAN","MIXED","MIXED_2","MIXED_3","SINGLE_LINE","DOUBLE_LINE","DIAGONAL","CROSS","WAVE","ZIG_ZAG","SCRATCH"] #
-#df_splits = ["SCRATCH"] 
 
-# experiment_trining = {
-#     "CLEAN": "800_96",
-#     "MIXED": "800_132",
-#     "DIAGONAL": "800_134",
-#     "WAVE": "800_138",
-#     "ZIG_ZAG": "800_139",
-#     "SCRATCH": "800_136",
-#     "CROSS": "800_133",
-#     "SINGLE_LINE": "800_137",
-#     "DOUBLE_LINE": "800_135",
-#     "Combo_WSD": "800_140"
-# }
-
-# experiment_trining = {
-#     "CLEAN": "167",
-#     "CLEAN": "168",
-#     "CLEAN": "169",
-#     "MIXED": "170",
-#     "MIXED": "171",
-#     "MIXED": "172",
-#     "SINGLE_LINE": "102",
-#     "SINGLE_LINE": "174",
-#     "DOUBLE_LINE": "100",
-#     "DOUBLE_LINE": "175",
-#     "DIAGONAL": "99",
-#     "DIAGONAL": "176",
-#     "CROSS": "98",
-#     "CROSS": "177",
-#     "WAVE": "103",
-#     "WAVE": "178",
-#     "ZIG_ZAG": "104",
-#     "ZIG_ZAG": "179",
-#     "SCRATCH": "101",
-#     "SCRATCH": "180",
-# }
+The input for config_probability2_striked_types should be a dictionary with the following keys
 
 experiment_trining = {
     "167": "CLEAN",
@@ -290,20 +225,11 @@ experiment_trining = {
     "213_2": "SCRATCH",
     "213_3": "SCRATCH"
 }
+config_probability2_clean = 0.0
 
-# experiment_trining = {
-#     "200": "SINGLE_LINE",
-#     "201": "DOUBLE_LINE",
-#     "202": "DIAGONAL",
-#     "203": "CROSS",
-#     "204": "WAVE",
-#     "205": "ZIG_ZAG",
-#     "206": "SCRATCH",
-# }
+"""
 
 
-main_cer = []
-main_wer = []
 
 config = parse_args(f"configs/config_800_207.yml")
 saved_directory = f"../../HTR-best-practices-strike2/results/saved_models_800_207_1"
@@ -315,82 +241,3 @@ test_dataset = "SINGLE_LINE"
 results_test = test_all_models(saved_models, 'test')
 
 print(results_test)
-
-# #df = pd.DataFrame(columns= ["Trained_on", "Score", "Model_ID"] + df_splits)
-# df = pd.read_csv('results_types_all_cross.csv')
-
-# for case in experiment_trining:
-#     saved_directory = f"results/saved_models_800_{case}"
-#     print(f"Trained Model: {case}")
-
-#     row_cer = {"Trained_on": experiment_trining[case], "Score": "CER", "Model_ID": case}
-#     row_wer = {"Trained_on": experiment_trining[case], "Score": "WER", "Model_ID": case}
-#     p=0 #CER: p=0 | WER: p=1
-
-#     for idx, split in enumerate(df_splits):
-#         test_dataset = split
-#         if split == "CLEAN":
-#             config_probability2_clean = 0.0
-#             config_probability2_striked_types = {"CLEAN": 1.0}
-        
-#         if split == "MIXED": #This need to be run on te fixed Mixed set by dataloader2.py which is the "evaluate_table_strike_removed.py"
-#             config_probability2_clean = 0.0
-#             config_probability2_striked_types = {"MIXED": 1.0}
-
-#         if split == "MIXED_2": 
-#             config_probability2_clean = 0.0
-#             config_probability2_striked_types = {"MIXED_2": 1.0}        
-            
-#         if split == "MIXED_3": 
-#             config_probability2_clean = 0.0
-#             config_probability2_striked_types = {"MIXED_3": 1.0}
-
-#         if split == "SINGLE_LINE":
-#             config_probability2_clean = 0.0
-#             config_probability2_striked_types = {"SINGLE_LINE": 1.0}
-        
-#         if split == "DOUBLE_LINE":
-#             config_probability2_clean = 0.0
-#             config_probability2_striked_types = {"DOUBLE_LINE": 1.0}        
-        
-#         if split == "DIAGONAL":
-#             config_probability2_clean = 0.0
-#             config_probability2_striked_types = {"DIAGONAL": 1.0}
-
-#         if split == "CROSS":
-#             config_probability2_clean = 0.0
-#             config_probability2_striked_types = {"CROSS": 1.0}
-        
-#         if split == "WAVE":
-#             config_probability2_clean = 0.0
-#             config_probability2_striked_types = {"WAVE": 1.0}
-        
-#         if split == "ZIG_ZAG":
-#             config_probability2_clean = 0.0
-#             config_probability2_striked_types = {"ZIG_ZAG": 1.0}
-        
-#         if split == "SCRATCH":
-#             config_probability2_clean = 0.0
-#             config_probability2_striked_types = {"SCRATCH": 1.0}    
-        
-
-
-
-#         # config = parse_args(f"configs/config_800_{case}.yml")
-#         config = parse_args(f"configs/config_800_207.yml")
-
-#         print(split)
-#         print(config.probability.clean)
-#         print(config.probability.striked_types)
-
-#         results_test = test_all_models(saved_models, 'test')
-#         print(results_test)
-
-#         row_cer[split] = results_test[0]
-#         row_wer[split] = results_test[1]
-
-#     df.loc[len(df)] = row_cer
-#     df.loc[len(df)] = row_wer
-
-
-# df.to_csv('results_types_all_cross.csv', index=False)
